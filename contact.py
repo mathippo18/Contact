1abacbe8fe126acd5e4183b2144e927980bea5a3
mathippo18 <107137963+mathippo18@users.noreply.github.com>
2023-05-04 16:43:35 +0200

    Ajout de toutes les fonctions sauf search

--- contact.py
+++ contact.py
@@ -1,53 +1,206 @@
 import sys
 import sqlite3
-
 conn = sqlite3.connect('Contact.db')
 cur = conn.cursor()
 
-def Insert_into(Nom,Prenom,Surnom,Telephone,Email,Adresse):     
-    Insert_clients ='''INSERT INTO Contact VALUES(?,?,?,?,?,?)'''
-    inserer = (Nom, Prenom, Surnom, Telephone, Email, Adresse)
-    cur.execute(Insert_clients, inserer)
-    conn.commit()
-    cur.close()
-    conn.close()
+def Insert_into(Nom,Prenom,Surnom,Telephone,Email,Adresse):
+    try:
+        Insert_clients ='''INSERT INTO Contact VALUES(?,?,?,?,?,?)'''
+        inserer = (Nom, Prenom, Surnom, Telephone, Email, Adresse)
+        cur.execute(Insert_clients, inserer)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except sqlite3.Error as error:
+        print("Petit soucis !", error)
 
-def list():
-    cur.execute('''Select * from Contact''')
-    result = cur.fetchall()
-    print(result)
-    conn.commit()
-    cur.close()
-    conn.close()
+def list(): #Changer le style
+    try:
+        sql = '''Select * from Contact'''
+        result = cur.execute(sql)
+        result = cur.fetchall()
+        print(result)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except sqlite3.Error as error:
+        print("Petit soucis !", error)
+
+def man():
+    print("hello noobie\nPour te servir de l'application en ligne de commande")
+    print('Il y a plusieurs options tel que lister tout vos contacts, ajouter des contacts ou faire des recherches')
+    print('\n')
+    print('Il y a des exemples ci-dessous :' )   
+    print("'python3 contact.py new' permet d'ajouter un nouveau contact en renseignant les informations le concernant")
+    print("'python3 contact.py list' permet d'afficher toute la base de donnée")
+    print("'python3 contact.py search --by-phone' permet de rechercher un contact en fonction de son numéro de téléphone")
+    print("'python3 contact.py search --by-name' permet de rechercher un contact en fonction de son nom de famille")
+    print("'python3 contact.py search --by-email' permet de rechercher un contact en fonction de son adresse email")
+    print("'python3 contact.py search --by-address' permet de rechercher un contact en fonction de son adress")
+    print("'python3 contact.py search --by-nickname' permet de rechercher un contact en fonction de son surnom")
+    print("'python3 contact.py search --by-firstname' permet de rechercher un contact en fonction de son prénom")
+    
 
 #def select_number():
 
+#def select_email():
+
+#def select_firstname():
+
 #def select_name():
 
+#def select_nickname():
+
+#def select_adresse():
+
+def supprimer(Nom, Prenom, Surnom, Telephone, Email, Adresse):
+
+    try:
+        Delete ='''Delete from Contact where Nom = ? AND Prénom = ? AND Surnom = ? AND Téléphone=? AND Email=? AND Adresse=? '''
+        Suppr = (Nom, Prenom, Surnom, Telephone, Email, Adresse)
+        cur.execute(Delete, Suppr)
+        conn.commit()
+        cur.close()
+        conn.close()  
+    except sqlite3.Error as error:
+        print("Petit soucis !", error)
+
+def maj(Loca_update,New_data,Nom, Prenom, Surnom, Téléphone, Email, Adresse):
+    if Loca_update == "Nom":
+        Cmd ='''UPDATE Contact SET Nom = ? WHERE Nom = ? AND Prénom = ? AND Surnom = ? AND Téléphone=? AND Email=? AND Adresse=? '''
+    if Loca_update == "Prénom":
+        Cmd ='''UPDATE Contact SET Prénom = ? WHERE Nom = ? AND Prénom = ? AND Surnom = ? AND Téléphone=? AND Email=? AND Adresse=? '''
+    if Loca_update == "Surnom":
+        Cmd ='''UPDATE Contact SET Surnom = ? WHERE Nom = ? AND Prénom = ? AND Surnom = ? AND Téléphone=? AND Email=? AND Adresse=? '''
+    if Loca_update == "Téléphone":
+        Cmd ='''UPDATE Contact SET Téléphone = ? WHERE Nom = ? AND Prénom = ? AND Surnom = ? AND Téléphone=? AND Email=? AND Adresse=? '''
+    if Loca_update == "Email":
+        Cmd ='''UPDATE Contact SET Email = ? WHERE Nom = ? AND Prénom = ? AND Surnom = ? AND Téléphone=? AND Email=? AND Adresse=? '''
+    if Loca_update == "Adresse":
+        Cmd ='''UPDATE Contact SET Adresse = ? WHERE Nom = ? AND Prénom = ? AND Surnom = ? AND Téléphone=? AND Email=? AND Adresse=? '''
+    try:
+        Update = (New_data,Nom, Prenom, Surnom, Téléphone, Email, Adresse)
+        cur.execute(Cmd, Update)
+        conn.commit()
+        cur.close()
+        conn.close()  
+    except sqlite3.Error as error:
+        print("Petit soucis ! ", error)
 
 for arg in sys.argv:
-    print(arg)
-    if arg == "new":
-        Nom= input('Quel est le nom de votre contact :')
-        Prenom= input('Quel est le prénom de votre contact :')
-        Surnom= input('Quel est le surnom de votre contact :')
-        Telephone= input('Quel est le numéro de téléphone de votre contact :')
-        Email= input('Quel est le mail de votre contact :')
-        Adresse= input("Quel est l'adresse de votre contact :")
-        Insert_into(Nom,Prenom,Surnom,Telephone,Email,Adresse)    
-    
-    if arg == "list":
-        list()
+    try:
+        if sys.argv[1] == "new" :
+            Nom= input('Quel est le nom de votre contact : ')
+            Prenom= input('Quel est le prénom de votre contact : ')
+            Surnom= input('Quel est le surnom de votre contact :')
+            Telephone= input('Quel est le numéro de téléphone de votre contact : ')
+            Email= input('Quel est le mail de votre contact ')
+            Adresse= input("Quel est l'adresse de votre contact ")
+            Insert_into(Nom,Prenom,Surnom,Telephone,Email,Adresse)
+            print("Insertion réussie")
+            break
+              
+
+        if sys.argv[1] == "list" :
+            list()
+            break
+
+        if sys.argv[1] == "?":
+            man()
+            break
+        if sys.argv[1] == "delete":
+            Nom= input('Quel est le nom de votre contact :')
+            Prenom= input('Quel est le prénom de votre contact :')
+            Surnom= input('Quel est le surnom de votre contact :')
+            Telephone= input('Quel est le numéro de téléphone de votre contact :')
+            Email= input('Quel est le mail de votre contact :')
+            Adresse= input("Quel est l'adresse de votre contact :")
+            supprimer(Nom,Prenom,Surnom,Telephone,Email,Adresse)
+            print("Suppression réussie")
+            break
+            
+        if sys.argv[1] == "update":
+            Loca_update = input("Quel est la colonne a modifier (Nom/Prénom/Surnom/Téléphone/Email/Adresse): ")
+            New_data = input("Quel est la nouvelle donnée : ")
+            Nom= input('Quel est le nom de votre a mettre a jour : :')
+            Prenom= input('Quel est le prénom de votre contact a mettre a jour :')
+            Surnom= input('Quel est le surnom de votre contact a mettre a jour :')
+            Telephone= input('Quel est le numéro de téléphone de votre contact a mettre a jour : ')
+            Email= input('Quel est le mail de votre contact a mettre a jour : ')
+            Adresse= input("Quel est l'adresse de votre contact a mettre a jour : ")
+            maj(Loca_update, New_data, Nom, Prenom, Surnom, Telephone, Email, Adresse)
+            break
+        if sys.argv[1] == "search":
+            try:
+                if sys.argv[2] == "--by-name" :
+                    print("Par nom")
         
-    if arg == "search":
-        print("par tel")
-        exit()
+                if sys.argv[2] == "--by-tel" :
+                    print("Par téléphone")
+
+                if sys.argv[2] == "--by-email" :
+                    print("Par mail")
+
+                if sys.argv[2] == "--by-nickname" :
+                    print("Par surnom")
+
+                if sys.argv[2] == "--by-firstname" :
+                    print("Par prénom")
+
+                if sys.argv[2] == "--by-address" :
+                    print("Par nom")
+            except:
+                print ("Veuillez respecter les commandes ou veuillez entrer dans le mode intéractif ")
+    except:
+        print("Bienvenue dans le mode intéractif ")
+        print("Tapez 1 pour ajouter un contact ")
+        print("Tapez 2 pour lister vos contacts ")
+        print("Tapez 3 pour un ou des contacts ")
+        print("Tapez 4 pour voir l'aide ")
+        print("Tapez 5 pour supprimer un contact ")
+        print("Tapez 6 pour modifier un contact ")
+        choix = input("Que voulez vous faire ? : ")
+        if choix == "1":
+            Nom= input('Quel est le nom de votre contact : ')
+            Prenom= input('Quel est le prénom de votre contact : ')
+            Surnom= input('Quel est le surnom de votre contact : ')
+            Telephone= input('Quel est le numéro de téléphone de votre contact : ')
+            Email = input('Quel est le mail de votre contact : ')
+            Adresse= input("Quel est l'adresse de votre contact : ")
+            Insert_into(Nom,Prenom,Surnom,Telephone,Email,Adresse)
+            print("Insertion réussie")
+            break
+        if choix == "2":    
+            list()
+            break
+        #if choix == "3":
+            #search
+        if choix =="4":
+            man()
+            break
+        if choix =="5":
+            Nom= input('Quel est le nom de votre contact a supprimer: ')
+            Prenom= input('Quel est le prénom de votre contact a supprimer: ')
+            Surnom= input('Quel est le surnom de votre contact a supprimer: ')
+            Telephone= input('Quel est le numéro de téléphone de votre contact a supprimer: ')
+            Email= input('Quel est le mail de votre contact a supprimer: ')
+            Adresse= input("Quel est l'adresse de votre contact a supprimer: ")
+            supprimer(Nom,Prenom,Surnom,Telephone,Email,Adresse)
+            print("Suppression réussie")
+            break
+        if choix =="6":
+            Loca_update = input("Quel est la colonne a modifier (Nom/Prénom/Surnom/Téléphone/Email/Adresse): ")
+            New_data = input("Quel est la nouvelle donnée : ")
+            Nom= input('Quel est le nom de votre a mettre a jour : :')
+            Prenom= input('Quel est le prénom de votre contact a mettre a jour :')
+            Surnom= input('Quel est le surnom de votre contact a mettre a jour :')
+            Telephone= input('Quel est le numéro de téléphone de votre contact a mettre a jour : ')
+            Email= input('Quel est le mail de votre contact a mettre a jour : ')
+            Adresse= input("Quel est l'adresse de votre contact a mettre a jour : ")
+            maj(Loca_update, New_data, Nom, Prenom, Surnom, Telephone, Email, Adresse)
+            break
+        
+   
 
 
-#https://pythonforge.com/sqlite/
-#https://waytolearnx.com/2020/06/inserer-des-donnees-dans-une-table-sqlite-avec-python.html
-
-#if len(sys.argv) == "Test":
-#    print ("Test fonctionne")
-#    exit()
 
